28ecb6da76e4605b7335ce78c7d4fb5867e46244
Vikesh S <vikesh024@gmail.com>
2026-09-05 16:31:03 +0530

    fix: Return code 1 if there are malformed lines

--- automation/invfilter/invfilter.py
+++ automation/invfilter/invfilter.py
@@ -3,9 +3,10 @@ import json
 import sys
 
 
-def filter_sku(log_lines, sku, min_abs_delta) -> list[dict]:
+def filter_sku(log_lines, sku, min_abs_delta) -> tuple[list[dict], int]:
 
     filtered_log_lines = []
+    malformed_lines = 0
     for line_number, line in enumerate(log_lines):
 
         if line.strip():
@@ -14,18 +15,14 @@ def filter_sku(log_lines, sku, min_abs_delta) -> list[dict]:
 
                 if j_line["sku"] == sku and j_line["delta"] >= min_abs_delta:
                     filtered_log_lines.append(j_line)
-            except json.JSONDecodeError as e:
+            except (json.JSONDecodeError, TypeError, KeyError) as e:
                 print(
-                    f"Malformed line at line number: {line_number + 1}, {e.doc}",
+                    f"Malformed line at line number: {line_number + 1}, {e}",
                     file=sys.stderr,
                 )
-            except TypeError as e:
-                print(
-                    f"Invalid log format at line number: {line_number}, {e}",
-                    file=sys.stderr,
-                )
+                malformed_lines += 1
 
-    return filtered_log_lines
+    return filtered_log_lines, malformed_lines
 
 
 def main():
@@ -33,10 +30,15 @@ def main():
     parser.add_argument("--sku-prefix", required=True)
     parser.add_argument("--min-abs-delta", type=int, default=0)
     args = parser.parse_args()
-    filtered_lines = filter_sku(sys.stdin, args.sku_prefix, args.min_abs_delta)
+    filtered_lines, malformed_line = filter_sku(
+        sys.stdin, args.sku_prefix, args.min_abs_delta
+    )
 
     for line in filtered_lines:
-        print(line)
+        print(json.dumps(line))
+
+    if malformed_line:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
